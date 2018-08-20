a13fa844198fcd019d92e3c7adfe2f6f1f2f4c39
datdta95 <datdta95@gmail.com>
2018-08-20 11:14:50 +0700

    Tìm giá trị lớn nhất với đầu vào là mảng Random

--- src/main/java/MaxValueFinding.java
+++ src/main/java/MaxValueFinding.java
@@ -1,11 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class MaxValueFinding {
     public static void main(String[] args) {
         int[][] array = generateTwoDimensionalArray();
+        System.out.println("Mảng Random:");
+        displayArray(array);
         System.out.println("Giá trị lớn nhất trong mảng là: " + findMaxValue(array));
     }
 
+    private static void displayArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int getNumberOfRowsArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số hàng của mảng: ");
+        return scanner.nextInt();
+    }
+
+    private static int getNumberOfColumnsArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số cột của mảng: ");
+        return scanner.nextInt();
+    }
+
     private static int[][] generateTwoDimensionalArray() {
-        int[][] array = {{12,14},{1,1}};
+        Random random = new Random();
+        int[][] array = new int[getNumberOfRowsArray()][getNumberOfColumnsArray()];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(100) + 1;
+            }
+        }
         return array;
     }
 
